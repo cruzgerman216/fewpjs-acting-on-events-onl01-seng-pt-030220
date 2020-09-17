2d5a0a6349ae2f07928ed8be59663d54b0dce74f
German Cruz <cruzgerman216@yahoo.com>
2020-09-17 02:09:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  let rightNumbers = dodger.style.right.replace("px", "");
+  let rightNumbers = dodger.style.left.replace("px", "");
   let right = parseInt(rightNumbers, 10);
   console.log(right);
 
