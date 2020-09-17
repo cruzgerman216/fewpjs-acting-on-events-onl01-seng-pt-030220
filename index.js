10c74a954fb05673a5ac224a8288c74ce2d993b1
German Cruz <cruzgerman216@yahoo.com>
2020-09-17 02:06:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ function moveDodgerLeft() {
   }
 }
 
+function moveDodgerRight() {
+  let rightNumbers = dodger.style.right.replace("px", "");
+  let right = parseInt(rightNumbers, 10);
+ 
+  if (right > 0) {
+    dodger.style.right = `${right + 1}px`;
+  }
+}
+
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
