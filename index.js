eea980fc7837200ff50b6188889eaad6abd03562
German Cruz <cruzgerman216@yahoo.com>
2020-09-17 02:06:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,7 @@ function moveDodgerRight() {
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
+  }else if(e.key ==="ArrowRight"){
+    moveDodgerRight();
   }
 });
